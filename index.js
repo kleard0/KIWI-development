e830aa17e263a866f43757865de06822ae5f42c0
danielOkpe <145658821+danielOkpe@users.noreply.github.com>
2024-04-29 06:16:42 +0200

    effet au scroll

--- index.js
+++ index.js
@@ -15,3 +15,28 @@ window.addEventListener("scroll", function (e) {
 
   observer.observe(resumeHearder);
 });
+
+const selection = Splitting();
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(selection[0].chars, {
+  color: "white",
+  stagger: 0.05,
+  scrollTrigger: {
+    trigger: ".text-reveal",
+    start: "top 97%",
+    end: "bottom 100%",
+    scrub: true,
+  },
+});
+
+const lenis = new Lenis();
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 900);
+});
+
+gsap.ticker.lagSmoothing(0);
